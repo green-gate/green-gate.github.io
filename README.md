4cc05fccf4d923bbe72098789b7b4477e6ece603
poli <kevinjosephpoli@gmail.com>
2017-07-23 15:51:23 -0400

    new build system, new site, new me

--- README.md
+++
@@ -1,2 +0,0 @@
-# chapter-site
-The Official Website for the Chi Phi Fraternity Mu Chapter
---
+++ js/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/*****************************************
+ * Place here your custom javascript code
+ **************************************/
+
+// Enable Bootstrap 4 Tooltips
+$('[data-toggle="tooltip"]').tooltip()
+
